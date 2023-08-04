2880c09d05f434128d455c736e5d642384c687a7
TalAharon23 <talaha@mta.ac.il>
2023-08-04 17:08:25 +0300

    Add terraform code and activemq

--- HelloWorldApp.py
+++ HelloWorldApp.py
@@ -1,13 +1,41 @@
+import time
+import stomp
 from flask import Flask
 
-app = Flask(__name__)
+def create_flask_app():
+    app = Flask(__name__)
+    
+    @app.route('/')
+    def hello():
+        return 'Hello World!'
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+    return app
+
+class ActiveMQConsumer(stomp.ConnectionListener):
+    def __init__(self):
+        self.conn = stomp.Connection()
+        self.conn.set_listener('', self)
+        self.conn.connect('admin', 'password', wait=True)
+        self.conn.subscribe(destination='/queue/app-queue', id=1, ack='auto')
+
+    def on_error(self, message):
+        print('received an error "%s"' % message.body)
+        
+    def on_message(self, message):
+        print('received a message "%s"' % message.body)
+        
+    def disconnect(self):
+        self.conn.disconnect()
 
 def main():
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app = create_flask_app()
+    consumer = ActiveMQConsumer()
+
+    try:
+        consumer.conn.send(body='Hello World!', destination='/queue/app-queue')
+        app.run(host='0.0.0.0', port=80, debug=True)
+    finally:
+        consumer.disconnect()
 
 if __name__ == '__main__':
     main()
---
+++ main.tf
@@ -0,0 +1,91 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  # host = "https://127.0.0.1:55664"
+}
+
+resource "kubernetes_namespace" "app_namespace" {
+  metadata {
+    name = "helloworld"
+  }
+}
+
+resource "kubernetes_deployment" "app_deployment" {
+  metadata {
+    name      = "helloworld-deployment"
+    namespace = kubernetes_namespace.app_namespace.metadata[0].name
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "hello_world_app"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "hello_world_app"
+        }
+      }
+
+      spec {
+        container {
+          name  = "helloworld-app"
+          image = "talaharon23/helloworld-app:1.0"
+
+          port {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "app_service" {
+  metadata {
+    name      = "helloworld-service"
+    namespace = kubernetes_namespace.app_namespace.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "hello_world_app"
+    }
+
+    port {
+      port        = 8080
+      target_port = 80
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "app_ingress" {
+  metadata {
+    name = "helloworld-ingress"
+  }
+
+  spec {
+    ingress_class_name = "nginx"
+    rule {
+      http {
+        path {
+          path = "/*"
+          path_type = "ImplementationSpecific"
+          backend {
+            service {
+              name = kubernetes_service.app_service.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
